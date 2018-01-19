f2ac0f2957b8b36967bc56e76484e84e82bd5a52
Fredrik Bore <fredrik@bore.ai>
2018-01-19 18:37:58 +0100

    First version of data loader done. More comments and small changes all over.

--- data_loader.py
+++ data_loader.py
@@ -1,17 +1,18 @@
 import csv
 import os
+import random
 import sys
+from typing import Tuple, Dict
 
 import cv2
 import matplotlib.pyplot as plt
-import tifffile
 import numpy as np
 import pandas as pd
-import webcolors
-
-import shapely.wkt
 import shapely.affinity
-from tqdm import tqdm
+import shapely.wkt
+import tifffile
+import webcolors
+from shapely.geometry import MultiPolygon
 
 csv.field_size_limit(sys.maxsize)
 
@@ -95,16 +96,78 @@ def scale_image_percentile(matrix):
 
 
 class Generator:
-    def __init__(self, data_path: str = "data", augment: bool = True):
+    """
+    Class responsible for generating batches of data to train on
+    """
+
+    def __init__(self, data_path: str = "data", batch_size: int = 10, patch_size: int = 572, augment: bool = True):
         self.data_path = data_path
         self.augment = augment
+        self.batch_size = batch_size
+        self.patch_size = patch_size
 
         # TODO: Pre-fetch image sizes
         self.grid_sizes = pd.read_csv(os.path.join(self.data_path, 'grid_sizes.csv'), index_col=0)
         self.training_image_ids = [f for f in os.listdir(os.path.join(self.data_path, "train_geojson_v3"))
                                    if os.path.isdir(os.path.join(os.path.join(self.data_path, "train_geojson_v3"), f))]
 
-    def get_grid_size(self, image_number: str):
+    def next(self) -> Tuple[any, any]:
+        """
+        Returns next batch of training images
+        Tuple(x_train, y_train)
+        x_train is a numpy array of shape [w, h, c]
+        y_train is a numpy array of shape [w, h]
+        """
+        # Extract a random subset of images from training pool (batch size)
+        training_image_ids = random.sample(self.training_image_ids, self.batch_size)
+        x_train_batch = []
+        y_train_batch = []
+
+        for training_image_id in training_image_ids:
+            x_train_temp = self.read_image(training_image_id)
+            y_train_temp = self.get_ground_truth_array(training_image_id)
+
+            if x_train_temp.shape[:2] != y_train_temp.shape:
+                raise Exception("Shape of data does not match shape of ground truth")
+
+            # Crop to patch size
+            start_index_width = np.random.randint(0, x_train_temp.shape[0] - self.patch_size)
+            start_index_height = np.random.randint(0, x_train_temp.shape[1] - self.patch_size)
+
+            x_train_temp = x_train_temp[start_index_width:start_index_width + self.patch_size,
+                                        start_index_height:start_index_height + self.patch_size]
+
+            y_train_temp = y_train_temp[start_index_width:start_index_width + self.patch_size,
+                                        start_index_height:start_index_height + self.patch_size]
+
+            # Augment
+            if self.augment:
+                num_rotations = np.random.randint(4)
+                flip_vertical = np.random.choice([True, False])
+                flip_horizontal = np.random.choice([True, False])
+
+                x_train_temp = np.rot90(x_train_temp, num_rotations)
+                y_train_temp = np.rot90(y_train_temp, num_rotations)
+
+                if flip_horizontal:
+                    x_train_temp = np.fliplr(x_train_temp)
+                    y_train_temp = np.fliplr(y_train_temp)
+
+                if flip_vertical:
+                    x_train_temp = np.flipud(x_train_temp)
+                    y_train_temp = np.flipud(y_train_temp)
+
+                print(f"{num_rotations} Vert {flip_vertical} Hori {flip_horizontal}")
+
+            x_train_batch.append(x_train_temp)
+            y_train_batch.append(y_train_temp)
+
+        return x_train_batch, y_train_batch
+
+    def get_grid_size(self, image_number: str) -> Tuple[float, float]:
+        """
+        Returns the grid size of a specific image. Needed to scale some coords
+        """
         return self.grid_sizes[image_number]
 
     def read_image(self, image_number: str, band: int = 3):
@@ -132,19 +195,24 @@ class Generator:
         Saves a image number from specified band and saves it to filename
         Overwrites existing files without warning
         """
-        overlay_data = self.get_ground_truth_polys(image_number)
-        train_mask = self.mask_for_polygons(overlay_data, image_number)
+        train_mask = self.mask_for_polygons(image_number)
         plt.imsave(filename, train_mask)
 
-    def scale_coords(self, img_size, image_number: str):
-        """Scale the coordinates of a polygon into the image coordinates for a grid cell"""
+    def scale_coords(self, img_size: Tuple[int, int], image_number: str) -> Tuple[float, float]:
+        """
+        Get a scaling factor needed to scale polygons to same size as image
+        """
         x_max, y_min = self.grid_sizes.loc[image_number][['Xmax', 'Ymin']]
         h, w = img_size
         w_ = w * (w / (w + 1))
         h_ = h * (h / (h + 1))
         return w_ / x_max, h_ / y_min
 
-    def get_ground_truth_polys(self, image_number: str):
+    def get_ground_truth_polys(self, image_number: str) -> Dict[str, MultiPolygon]:
+        """
+        Get a list of polygons sorted by class for the selected image.
+        Scaled to match image.
+        """
         train_polygons = dict()
         for _im_id, _poly_type, _poly in csv.reader(open(os.path.join(self.data_path, 'train_wkt_v4.csv'))):
             if _im_id == image_number:
@@ -159,20 +227,49 @@ class Generator:
 
         return train_polygons_scaled
 
-    def mask_for_polygons(self, polygons, image_number: str):
+    def get_ground_truth_array(self, image_number: str):
+        """
+        Creates a array containing class for each pixel
+        """
+        w, h = self.read_image(image_number).shape[:2]
+        polygons = self.get_ground_truth_polys(image_number)
+
+        # White background
+        img_mask = np.full((w, h), 0, np.uint8)
+
+        # Sort polygons by Z-order
+        for cls, _ in sorted(ZORDER.items(), key=lambda x: x[1]):
+            exteriors = [np.array(poly.exterior.coords).round().astype(np.int32)
+                         for poly in polygons[str(cls)]]
+
+            cv2.fillPoly(img_mask, exteriors, int(cls))
+
+            # Some polygons have regions inside them which need to be excluded
+            interiors = [np.array(pi.coords).round().astype(np.int32)
+                         for poly in polygons[str(cls)] for pi in poly.interiors]
+
+            cv2.fillPoly(img_mask, interiors, 0)
+
+        return img_mask
+
+    def mask_for_polygons(self, image_number: str):
         """
         Create a color mask of classes with the same size as original image
         """
         w, h = self.read_image(image_number).shape[:2]
+        polygons = self.get_ground_truth_polys(image_number)
 
+        # White background
         img_mask = np.full((w, h, 3), 255, np.uint8)
 
         # Sort polygons by Z-order
         for cls, _ in sorted(ZORDER.items(), key=lambda x: x[1]):
             exteriors = [np.array(poly.exterior.coords).round().astype(np.int32) for poly in polygons[str(cls)]]
+            cv2.fillPoly(img_mask, exteriors, webcolors.hex_to_rgb(COLOR_MAPPING[int(cls)]))
+
+            # Some polygons have regions inside them which need to be excluded
             interiors = [np.array(pi.coords).round().astype(np.int32)
                          for poly in polygons[str(cls)] for pi in poly.interiors]
-            cv2.fillPoly(img_mask, exteriors, webcolors.hex_to_rgb(COLOR_MAPPING[int(cls)]))
             cv2.fillPoly(img_mask, interiors, (255, 255, 255))
 
         return img_mask
@@ -180,7 +277,8 @@ class Generator:
 
 if __name__ == "__main__":
     generator = Generator()
+    x_train, y_train = generator.next()
 
-    for training_image in tqdm(os.listdir("data/train_geojson_v3"), desc="Generating images", unit="img"):
-        generator.save_image(training_image, f"images/{training_image}.png")
-        generator.save_overlay(training_image, f"images/{training_image}_overlay.png")
+    # for training_image in tqdm(os.listdir("data/train_geojson_v3"), desc="Generating images", unit="img"):
+    #    generator.save_image(training_image, f"images/{training_image}.png")
+    #    generator.save_overlay(training_image, f"images/{training_image}_overlay.png")
