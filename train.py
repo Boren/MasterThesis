fb2bb9d3ef466eb1347956612cbbe1eb0df1f2d8
andreas <taralds1@gmail.com>
2018-02-27 11:54:50 +0100

    Make FCN great again

--- train.py
+++ train.py
@@ -11,27 +11,26 @@ if __name__ == "__main__":
     # TODO: Load arguments from command line
     num_classes = 10
     input_size = 160
-    epochs = 100
-    batch_size = 1
-    val_amount = 1
+    epochs = 1000
+    batch_size = 1000
+    val_amount = 50
 
-    os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
 
     generator = Generator(patch_size=input_size, batch_size=batch_size)
 
-    model = unet.unet(input_size=input_size, num_classes=num_classes)
+    model = fcn.fcn32(input_size=input_size, num_classes=num_classes)
 
-    if os.path.isfile('weights/unet.hdf5'):
+    if os.path.isfile('weights/fcn.hdf5'):
         load = input("Saved weights found. Load? (y/n)")
         if load == "y":
             print("Loading saved weights")
-            model.load_weights('weights/unet.hdf5')
+            model.load_weights('weights/fcn.hdf5')
     model.summary()
-    plot_model(model, show_shapes=False, to_file="images/unet_model.png")
-    plot_model(model, show_shapes=True, to_file="images/unet_model_shapes.png")
+    plot_model(model, show_shapes=False, to_file="images/fcn_model.png")
+    plot_model(model, show_shapes=True, to_file="images/fcn_model_shapes.png")
     if not os.path.exists('weights'):
         os.makedirs('weights')
-    model_checkpoint = ModelCheckpoint('weights/unet.hdf5', monitor='loss', save_best_only=True)
+    model_checkpoint = ModelCheckpoint('weights/fcn.hdf5', monitor='loss', save_best_only=True)
 
     # Setup tensorboard model
     tbCallBack = TensorBoard(log_dir='tensorboard_log/', histogram_freq=1,
