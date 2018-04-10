1505cd94b82c898201b59b8d577041239a0d31e9
Fredrik Bore <fredrik@bore.ai>
2018-04-10 12:20:31 +0200

    Charizard (Tiramisu :D)

--- data_loader.py
+++ data_loader.py
@@ -11,7 +11,7 @@ import shapely.wkt
 import tifffile
 from shapely.geometry import MultiPolygon
 
-csv.field_size_limit(2**24)
+csv.field_size_limit(2 ** 24)
 
 
 def scale_image_percentile(matrix):
@@ -71,21 +71,21 @@ class Generator:
             os.makedirs(cache_folder)
 
         for image_id in self.all_image_ids:
-            cache_path = os.path.join(cache_folder, "{image_id}".format(image_id=image_id))
+            cache_path = os.path.join(cache_folder, "{}".format(image_id))
             img_width = None
             img_height = None
             temp_data_x = None
             temp_data_y = None
 
             if not os.path.isfile(cache_path + "_x.npy"):
-                print("Caching image {image_id}".format(image_id=image_id))
+                print("Caching image {}".format(image_id))
                 temp_data_x = self.read_image(image_id)
                 img_width = temp_data_x.shape[0]
                 img_height = temp_data_x.shape[1]
                 np.save(cache_path + "_x", temp_data_x)
 
             if not os.path.isfile(cache_path + "_y.npy"):
-                print("Caching ground truth {image_id}".format(image_id=image_id))
+                print("Caching ground truth {}".format(image_id))
 
                 # In case image is not loaded we have to load to get dimensions
                 if img_width is None:
@@ -96,7 +96,8 @@ class Generator:
                 temp_data_y = np.zeros((img_width, img_height, 10))
                 polygons = self.get_ground_truth_polys(image_id)
                 for z in range(10):
-                    temp_data_y[:, :, z] = self.get_ground_truth_array(polygons, z + 1, (img_width, img_height))
+                    temp_data_y[:, :, z] = self.get_ground_truth_array(
+                        polygons, z + 1, (img_width, img_height))
                 np.save(cache_path + "_y", temp_data_y)
 
             if self.cache_in_memory:
@@ -135,21 +136,32 @@ class Generator:
                 x_train_temp = self.cache_x[image_id]
                 y_train_temp = self.cache_y[image_id]
             else:
-                x_train_temp = np.load(os.path.join(self.data_path, "cache", "{image_id}_x.npy".format(image_id=image_id)), mmap_mode='r')
-                y_train_temp = np.load(os.path.join(self.data_path, "cache", "{image_id}_y.npy".format(image_id=image_id)), mmap_mode='r')
+                x_train_temp = np.load(os.path.join(self.data_path, "cache",
+                                                    "{}_x.npy".format(
+                                                        image_id)),
+                                       mmap_mode='r')
+                y_train_temp = np.load(os.path.join(self.data_path, "cache",
+                                                    "{}_y.npy".format(
+                                                        image_id)),
+                                       mmap_mode='r')
 
             if x_train_temp.shape[:2] != y_train_temp.shape[:2]:
-                raise Exception("Shape of data does not match shape of ground truth")
+                raise Exception(
+                    "Shape of data does not match shape of ground truth")
 
             # Crop to patch size
-            start_index_width = np.random.randint(0, x_train_temp.shape[0] - self.patch_size)
-            start_index_height = np.random.randint(0, x_train_temp.shape[1] - self.patch_size)
+            start_index_width = np.random.randint(0, x_train_temp.shape[
+                0] - self.patch_size)
+            start_index_height = np.random.randint(0, x_train_temp.shape[
+                1] - self.patch_size)
 
-            x_train_temp = x_train_temp[start_index_width:start_index_width + self.patch_size,
-                                        start_index_height:start_index_height + self.patch_size]
+            x_train_temp = x_train_temp[
+                           start_index_width:start_index_width + self.patch_size,
+                           start_index_height:start_index_height + self.patch_size]
 
-            y_train_temp = y_train_temp[start_index_width:start_index_width + self.patch_size,
-                                        start_index_height:start_index_height + self.patch_size]
+            y_train_temp = y_train_temp[
+                           start_index_width:start_index_width + self.patch_size,
+                           start_index_height:start_index_height + self.patch_size]
 
             # Augment
             if self.augment:
@@ -174,8 +186,12 @@ class Generator:
         return np.array(x_train_batch), np.array(y_train_batch)
 
     def get_patch(self, image: str, x: int, y: int, width: int, height: int):
-        x_train = np.load(os.path.join(self.data_path, "cache", "{image_id}_x.npy".format(image_id=image)))
-        y_train = np.load(os.path.join(self.data_path, "cache", "{image_id}_y.npy".format(image_id=image)))
+        x_train = np.load(os.path.join(self.data_path, "cache",
+                                       "{image_id}_x.npy".format(
+                                           image_id=image)))
+        y_train = np.load(os.path.join(self.data_path, "cache",
+                                       "{image_id}_y.npy".format(
+                                           image_id=image)))
 
         x_train = x_train[x:x + width, y:y + height]
         y_train = y_train[x:x + width, y:y + height]
@@ -188,23 +204,27 @@ class Generator:
 
     def get_grid_size(self, image_number: str) -> Tuple[float, float]:
         """
-        Returns the grid size of a specific image. Needed to scale some coords
+        Returns the grid size of a specific image.
+        Needed to scale some coords.
         """
         return self.grid_sizes[image_number]
 
     def read_image(self, image_id: str, band: int = 3):
         """
-        Reads a image number from specified band and stores the image in a numpy array
+        Reads a image number from specified band.
+        Stores the image in a numpy array.
         """
         if band == 3:
-            filename = os.path.join(self.data_path, "three_band", '{image_id}.tif'.format(image_id=image_id))
+            filename = os.path.join(self.data_path, "three_band",
+                                    '{}.tif'.format(image_id))
             raw_data = tifffile.imread(filename).transpose([1, 2, 0])
             image_data = scale_image_percentile(raw_data)
             return image_data
         else:
             raise Exception("Only 3-band is implemented")
 
-    def scale_coords(self, img_size: Tuple[int, int], image_number: str) -> Tuple[float, float]:
+    def scale_coords(self, img_size: Tuple[int, int], image_number: str) -> \
+            Tuple[float, float]:
         """
         Get a scaling factor needed to scale polygons to same size as image
         """
@@ -214,27 +234,34 @@ class Generator:
         h_ = h * (h / (h + 1))
         return w_ / x_max, h_ / y_min
 
-    def get_ground_truth_polys(self, image_number: str) -> Dict[str, MultiPolygon]:
+    def get_ground_truth_polys(self, image_number: str) -> \
+            Dict[str, MultiPolygon]:
         """
         Get a list of polygons sorted by class for the selected image.
         Scaled to match image.
         """
         train_polygons = dict()
-        for _im_id, _poly_type, _poly in csv.reader(open(os.path.join(self.data_path, 'train_wkt_v4.csv'))):
+        for _im_id, _poly_type, _poly in csv.reader(
+                open(os.path.join(self.data_path, 'train_wkt_v4.csv'))):
             if _im_id == image_number:
                 train_polygons[_poly_type] = shapely.wkt.loads(_poly)
 
-        x_scale, y_scale = self.scale_coords(self.read_image(image_number).shape[:2], image_number)
+        x_scale, y_scale = self.scale_coords(
+            self.read_image(image_number).shape[:2], image_number)
 
         train_polygons_scaled = dict()
         for key, train_polygon in train_polygons.items():
-            train_polygons_scaled[key] = shapely.affinity.scale(train_polygon, xfact=x_scale, yfact=y_scale,
-                                                                origin=(0, 0, 0))
+            train_polygons_scaled[key] = \
+                shapely.affinity.scale(train_polygon,
+                                       xfact=x_scale,
+                                       yfact=y_scale,
+                                       origin=(0, 0, 0))
 
         return train_polygons_scaled
 
     @staticmethod
-    def get_ground_truth_array(polygons, class_number: int, image_size: Tuple[int, int]):
+    def get_ground_truth_array(polygons, class_number: int,
+                               image_size: Tuple[int, int]):
         """
         Creates a array containing class for each pixel
         """
@@ -250,7 +277,8 @@ class Generator:
 
         # Some polygons have regions inside them which need to be excluded
         interiors = [np.array(pi.coords).round().astype(np.int32)
-                     for poly in polygons[str(class_number)] for pi in poly.interiors]
+                     for poly in polygons[str(class_number)] for pi in
+                     poly.interiors]
 
         cv2.fillPoly(img_mask, interiors, 0)
 
--- models/tiramisu.py
+++ models/tiramisu.py
@@ -1,14 +1,12 @@
 from typing import Tuple
 
-import keras.models as models
-from keras import Model
+from keras import Input, Model
 from keras import backend as keras_backend
-from keras.layers import Conv2D, Conv2DTranspose
+from keras.layers import Conv2D, Conv2DTranspose, concatenate
 from keras.layers.convolutional import MaxPooling2D
-from keras.layers.core import Dropout, Activation
+from keras.layers.core import Dropout
 from keras.layers.normalization import BatchNormalization
 from keras.optimizers import Adam
-from keras.regularizers import l2
 from keras_contrib.losses import jaccard_distance
 
 from utils.metrics import dice_coefficient
@@ -22,58 +20,73 @@ def tiramisu(input_size: int, num_classes: int, channels: int = 3) -> \
 
     https://arxiv.org/abs/1611.09326
     https://github.com/0bserver07/One-Hundred-Layers-Tiramisu
+    https://github.com/SimJeg/FC-DenseNet
 
     103 layers - High memory requirements
     """
-    if input_size <= 8:
-        raise Exception("Input size too small")
+    n_filters_first_conv = 48
+    n_pool = 4
+    growth_rate = 12
+    n_layers_per_block = 5
+    n_layers_per_block = [n_layers_per_block] * (2 * n_pool + 1)
+    dropout_p = 0.2
 
-    model = models.Sequential()
+    inputs = Input((input_size, input_size, channels), name="Input")
 
-    model.add(Conv2D(48, kernel_size=(3, 3), padding='same',
-                     input_shape=(input_size, input_size, channels),
-                     kernel_initializer="he_uniform",
-                     kernel_regularizer=l2(0.0001)))
+    stack = Conv2D(n_filters_first_conv, kernel_size=(3, 3),
+                   padding='same')(inputs)
+    n_filters = n_filters_first_conv
 
-    model = dense_block(model, 4, 112)
-    model = transition_down(model, 112)
+    # Start downsampling
+    skip_connection_list = []
 
-    model = dense_block(model, 5, 192)
-    model = transition_down(model, 192)
+    for i in range(n_pool):
+        # Dense Block
+        with keras_backend.name_scope("Dense_Block"):
+            for j in range(n_layers_per_block[i]):
+                x = dense_block(stack, growth_rate, dropout_p=dropout_p)
+                stack = concatenate([stack, x])
+                n_filters += growth_rate
 
-    model = dense_block(model, 7, 304)
-    model = transition_down(model, 304)
+            skip_connection_list.append(stack)
 
-    model = dense_block(model, 10, 464)
-    model = transition_down(model, 464)
+        # Transition Down
+        with keras_backend.name_scope("Transition_Down"):
+            stack = transition_down(stack, n_filters, dropout_p)
 
-    model = dense_block(model, 12, 656)
-    model = transition_down(model, 656)
+    skip_connection_list = skip_connection_list[::-1]
 
-    model = dense_block(model, 15, 896)
+    # Bottleneck
+    block_to_upsample = []
 
-    model = transition_up(model, 1088, (1088, 7, 7))
-    model = dense_block(model, 12, 1088)
+    # Dense Block
+    with keras_backend.name_scope("Dense_Block"):
+        for j in range(n_layers_per_block[n_pool]):
+            x = dense_block(stack, growth_rate, dropout_p=dropout_p)
+            block_to_upsample.append(x)
+            stack = concatenate([stack, x])
 
-    model = transition_up(model, 816, (816, 14, 14))
-    model = dense_block(model, 10, 816)
+    # Begin upsampling
+    for i in range(n_pool):
+        # Transition Up ( Upsampling + concatenation with the skip connection)
+        n_filters_keep = growth_rate * n_layers_per_block[n_pool + i]
+        stack = transition_up(skip_connection_list[i], block_to_upsample,
+                              n_filters_keep)
 
-    model = transition_up(model, 576, (576, 28, 28))
-    model = dense_block(model, 7, 576)
+        # Dense Block
+        block_to_upsample = []
+        with keras_backend.name_scope("Dense_Block"):
+            for j in range(n_layers_per_block[n_pool + i + 1]):
+                x = dense_block(stack, growth_rate, dropout_p=dropout_p)
+                block_to_upsample.append(x)
+                stack = concatenate([stack, x])
 
-    model = transition_up(model, 384, (384, 56, 56))
-    model = dense_block(model, 5, 384)
+    # Classifier
+    with keras_backend.name_scope("Classification"):
+        x = Conv2D(num_classes, kernel_size=(1, 1), padding='same')(stack)
+        x = Conv2D(num_classes, (1, 1), activation='sigmoid')(x)
 
-    model = transition_up(model, 256, (256, 112, 112))
-    model = dense_block(model, 4, 256)
-
-    model.add(Conv2D(num_classes, kernel_size=(1, 1),
-                     padding='same',
-                     kernel_initializer="he_uniform",
-                     kernel_regularizer=l2(0.0001)))
-
-    model.add(Conv2D(num_classes, (1, 1), activation='sigmoid'))
-
+    model = Model(inputs=inputs, outputs=x)
     model.compile(optimizer=Adam(),
                   loss='binary_crossentropy',
                   metrics=[dice_coefficient, jaccard_distance, 'accuracy'])
@@ -81,40 +94,28 @@ def tiramisu(input_size: int, num_classes: int, channels: int = 3) -> \
     return model, "tiramisu"
 
 
-def dense_block(model, layers, filters):
-    with keras_backend.name_scope("Dense_Block"):
-        for i in range(layers):
-            model.add(BatchNormalization(axis=1,
-                                         gamma_regularizer=l2(0.0001),
-                                         beta_regularizer=l2(0.0001)))
-            model.add(Activation('relu'))
-            model.add(Conv2D(filters, kernel_size=(3, 3), padding='same',
-                             kernel_initializer="he_uniform"))
-            model.add(Dropout(0.2))
+def dense_block(x, filters, filter_size=3, dropout_p=0.2):
+    x = BatchNormalization()(x)
+    x = Conv2D(filters, kernel_size=(filter_size, filter_size),
+               padding='same', activation='relu')(x)
+    x = Dropout(dropout_p)(x)
 
-        return model
+    return x
 
 
-def transition_down(model, filters):
+def transition_down(x, n_filters, dropout_p=0.2):
     with keras_backend.name_scope("Transition_Down"):
-        model.add(BatchNormalization(axis=1,
-                                     gamma_regularizer=l2(0.0001),
-                                     beta_regularizer=l2(0.0001)))
-        model.add(Activation('relu'))
-        model.add(Conv2D(filters, kernel_size=(1, 1), padding='same',
-                         kernel_initializer="he_uniform"))
-        model.add(Dropout(0.2))
-        model.add(MaxPooling2D(pool_size=(2, 2),
-                               strides=(2, 2)))
+        x = dense_block(x, n_filters, filter_size=1, dropout_p=dropout_p)
+        x = MaxPooling2D(pool_size=(2, 2), strides=(2, 2))(x)
 
-        return model
+        return x
 
 
-def transition_up(model, filters, input_shape):
-    with keras_backend.name_scope("Transistion_Up"):
-        model.add(Conv2DTranspose(filters, kernel_size=(3, 3), strides=(2, 2),
-                                  padding='same',
-                                  input_shape=input_shape,
-                                  kernel_initializer="he_uniform"))
+def transition_up(skip_connection, block_to_upsample, n_filters_keep):
+    with keras_backend.name_scope("Transition_Up"):
+        x = concatenate(block_to_upsample)
+        x = Conv2DTranspose(n_filters_keep, kernel_size=(3, 3), strides=(2, 2),
+                            padding='same')(x)
+        x = concatenate([x, skip_connection])
 
-        return model
+        return x
