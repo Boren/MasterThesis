f2a99df301564b7fdd101d5da9d82293d2cbb20e
Fredrik Bore <fredrik@bore.ai>
2018-04-11 11:56:01 +0200

    Wartortle

--- train.py
+++ train.py
@@ -6,6 +6,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import webcolors
 from PIL import Image
+from keras.utils import plot_model
 from keras.callbacks import ModelCheckpoint, TensorBoard
 
 from data_loader import Generator
@@ -28,9 +29,12 @@ def get_model(algorithm: str, input_size: int, num_classes: int):
     return model, model_name
 
 
-def create_directories():
+def create_directories(run_name: str):
     if not os.path.exists('images'):
         os.makedirs('images')
+
+    if not os.path.exists('images/{}'.format(run_name)):
+        os.makedirs('images/{}'.format(run_name))
 
     if not os.path.exists('weights'):
         os.makedirs('weights')
@@ -39,9 +43,8 @@ def create_directories():
         os.makedirs('tensorboard_log')
 
 
-def train(algorithm: str, input_size: int, epochs: int,
-          batch_size: int, num_classes: int = 10):
-    create_directories()
+def train(algorithm: str, input_size: int, epochs: int, batch_size: int,
+          num_classes: int = 10, verbose: bool = False):
 
     val_amount = max(batch_size // 10, 1)
 
@@ -50,21 +53,31 @@ def train(algorithm: str, input_size: int, epochs: int,
 
     model, model_name = get_model(algorithm, input_size, num_classes)
 
+    timenow = datetime.datetime.now().strftime("%Y.%m.%d_%H.%M.%S")
+    run_name = "{}_{}".format(model_name, timenow)
+    create_directories(run_name)
+
+    # TODO: Update with ability to choose weights
     if os.path.isfile('weights/{}.hdf5'.format(model_name)):
         load = input("Saved weights found. Load? (y/n)")
         if load == "y":
             print("Loading saved weights")
             model.load_weights('weights/{}.hdf5'.format(model_name))
-    model.summary()
+    if verbose:
+        model.summary()
 
-    timenow = datetime.datetime.now().strftime("%Y.%m.%d_%H.%M.%S")
-    model_checkpoint = ModelCheckpoint('weights/{}_{}.hdf5'.format(model_name, timenow), monitor='val_loss', save_best_only=True)
+    plot_model(model, os.path.join('images', run_name, 'model.png'))
+    plot_model(model, os.path.join('images', run_name, 'model_shapes.png'),
+               show_shapes=True)
+
+    model_checkpoint = \
+        ModelCheckpoint('weights/run_name.hdf5'.format(run_name),
+                        monitor='val_loss', save_best_only=True)
 
     # Setup tensorboard model
-    tensorboard_callback = TensorBoard(log_dir='tensorboard_log/{}_{}/'.format(model_name, timenow),
-                                       histogram_freq=0,
-                                       write_graph=True,
-                                       write_images=False)
+    tensorboard_callback = \
+        TensorBoard(log_dir='tensorboard_log/run_name/'.format(run_name),
+                    histogram_freq=0, write_graph=True, write_images=False)
 
     val_x, val_y = generator.next(amount=val_amount, data_type='validation')
 
@@ -74,7 +87,8 @@ def train(algorithm: str, input_size: int, epochs: int,
                         callbacks=[model_checkpoint, tensorboard_callback], validation_data=(val_x, val_y))
 
 
-def test(algorithm: str, input_size: int, num_classes: int = 10):
+def test(algorithm: str, input_size: int, num_classes: int = 10,
+         verbose: bool = False):
     test_image = '6060_2_3'
     generator = Generator(patch_size=input_size)
 
@@ -91,6 +105,9 @@ def test(algorithm: str, input_size: int, num_classes: int = 10):
         model.load_weights('weights/{}'.format(selected_weight))
     else:
         raise Exception("No weights")
+
+    create_directories(os.path.splitext(selected_weight)[0])
+    save_folder = os.path.join('images', os.path.splitext(selected_weight)[0])
 
     test_amount = 1
 
@@ -114,32 +131,40 @@ def test(algorithm: str, input_size: int, num_classes: int = 10):
         palette.extend(list(webcolors.hex_to_rgb(COLOR_MAPPING[int(i+1)])))
 
     result_img.putpalette(palette)
-    result_img.save(os.path.join("images", '{}_combined.png'.format(test_image)))
+    result_img.save(os.path.join(save_folder, '{}_combined.png'.format(test_image)))
 
     # Plot results
     print("Plotting results...")
     for patchnum in range(test_amount):
         plt.figure(figsize=(2000 / 96, 5000 / 96), dpi=96)
-        ax1 = plt.subplot(11, 2, 1)
+        ax1 = plt.subplot(11, 3, 1)
         ax1.set_title('Raw RGB data')
         ax1.imshow(test_x[patchnum, :, :, :], cmap=plt.get_cmap('gist_ncar'))
 
-        ax1 = plt.subplot(11, 2, 2)
+        ax1 = plt.subplot(11, 3, 2)
         ax1.set_title('Combined ground truth')
-        #ax1.imshow(mask_for_array(test_y[patchnum, :, :, :]), cmap=plt.get_cmap('gist_ncar'))
+        ax1.imshow(mask_for_array(test_y[patchnum, :, :, :]), cmap=plt.get_cmap('gist_ncar'))
+
+        ax1 = plt.subplot(11, 3, 3)
+        ax1.set_title('Combined predicition')
         ax1.imshow(result_img)
 
         for cls in range(num_classes):
-            ax2 = plt.subplot(11, 2, 2 * cls + 3)
+            ax2 = plt.subplot(11, 3, 3 * cls + 4)
             ax2.set_title('Ground Truth ({cls})'.format(cls=CLASS_TO_LABEL[cls + 1]))
             ax2.imshow(test_y[patchnum, :, :, cls], cmap=plt.get_cmap('Reds'))
 
-            ax3 = plt.subplot(11, 2, 2 * cls + 4)
+            ax3 = plt.subplot(11, 3, 3 * cls + 5)
             ax3.set_title('Prediction ({cls})'.format(cls=CLASS_TO_LABEL[cls + 1]))
             ax3.imshow(test_y_result[patchnum, :, :, cls], cmap=plt.get_cmap('Reds'),
                        interpolation='nearest')
+
+            ax4 = plt.subplot(11, 3, 3 * cls + 6)
+            ax4.set_title('Prediction ({cls})'.format(cls=CLASS_TO_LABEL[cls + 1]))
+            ax4.imshow(test_y_result[patchnum, :, :, cls], cmap=plt.get_cmap('Reds'),
+                       interpolation='nearest', vmin=0, vmax=1)
         plt.suptitle('{}'.format(algorithm))
-        plt.savefig(os.path.join("images", '{}.png'.format(test_image)))
+        plt.savefig(os.path.join(save_folder, '{}.png'.format(test_image)))
 
 
 def main():
@@ -160,20 +185,24 @@ def main():
     parser.add_argument("--test", dest='test', action='store_true',
                         help="Run a test")
 
-    parser.set_defaults(test=False)
+    parser.add_argument("--verbose", dest='verbose', action='store_true',
+                        help="Show additional debug information")
+
+    parser.set_defaults(test=False, verbose=False)
     args = parser.parse_args()
 
     algorithm = args.algorithm
     input_size = args.size
     epochs = args.epochs
     batch_size = args.batch
+    verbose = args.verbose
 
     num_classes = 10
 
     if args.test:
-        test(algorithm, input_size, num_classes)
+        test(algorithm, input_size, num_classes, verbose)
     else:
-        train(algorithm, input_size, epochs, batch_size, num_classes)
+        train(algorithm, input_size, epochs, batch_size, num_classes, verbose)
 
 
 if __name__ == "__main__":
