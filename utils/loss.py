967073d29945bb6c2f1962cd30ae1fef2387ca05
Fredrik Bore <fredrikbore@gmail.com>
2018-05-09 11:38:24 +0200

    More work on loss

--- utils/loss.py
+++ utils/loss.py
@@ -1,4 +1,5 @@
 import tensorflow as tf
+from keras.losses import binary_crossentropy
 
 
 def dice_loss(y_true, y_pred):
@@ -12,4 +13,24 @@ def jaccard_loss(y_true, y_pred):
     intersection = tf.reduce_sum(tf.multiply(y_true, y_pred))
     union = tf.reduce_sum(y_true) + tf.reduce_sum(y_pred) - intersection
     loss = 1. - intersection / (union + tf.constant(tf.keras.backend.epsilon()))
+    return loss
+
+
+def ce_jaccard_loss(y_true, y_pred):
+    ce_loss = binary_crossentropy(y_true, y_pred)
+
+    intersection = tf.reduce_sum(tf.multiply(y_true, y_pred))
+    union = tf.reduce_sum(y_true) + tf.reduce_sum(y_pred) - intersection
+    jaccard_loss = - tf.log((intersection + tf.constant(tf.keras.backend.epsilon())) / (union + tf.constant(tf.keras.backend.epsilon())))
+    loss = ce_loss + jaccard_loss
+    return loss
+
+
+def ce_dice_loss(y_true, y_pred):
+    ce_loss = binary_crossentropy(y_true, y_pred)
+
+    intersection = tf.reduce_sum(tf.multiply(y_true, y_pred))
+    union = tf.reduce_sum(tf.square(y_true)) + tf.reduce_sum(tf.square(y_pred))
+    dice_loss = - tf.log((intersection + tf.constant(tf.keras.backend.epsilon())) / (union + tf.constant(tf.keras.backend.epsilon())))
+    loss = ce_loss + dice_loss
     return loss
